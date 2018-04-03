30b3deaa3db7a9abc83baac178db157d18331283
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2018-04-03 09:17:12 +0530

    reformatted

--- pythonbeyond/decorators/callcount.py
+++ pythonbeyond/decorators/callcount.py
@@ -1,41 +1,41 @@
 '''
 The below example demonstrates how decorators can be implemented using classes,
-instances. how multiple decorators can  be chained
-
+instances ,how  to use multiple decorators
 '''
 
-class  CallCount:
-    def __init__(self,func):
+
+class CallCount:
+    def __init__(self, func):
         self.func = func
-        self.count =0
-    
-    def __call__(self,*args,**kwargs):
-        self.count  +=1
-        return self.func(*args,**kwargs)
+        self.count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        return self.func(*args, **kwargs)
+
 
 class Tracer:
     def __init__(self):
-        self.enabled =True
-   
-    
-    def __call__(self,f):
-        def wrap(*args,**kwargs):
+        self.enabled = True
+
+    def __call__(self, f):
+        def wrap(*args, **kwargs):
             if self.enabled:
                 print("From {0},calling function {1}".format(
                       self.__class__.__name__,
                       f.__name__))
-            return  f(*args,**kwargs)
+            return f(*args, **kwargs)
         return wrap
 
+
 tracer = Tracer()
-
-
 
 
 @CallCount
 @tracer
 def hello(name):
     print("Hello {0}".format(name))
+
 
 if __name__ == '__main__':
     hello("Krishna")
