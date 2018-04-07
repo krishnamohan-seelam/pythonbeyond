d0973aa6578b21307ee3a7e95e8e66e9d64aa064
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2018-04-07 13:41:57 +0530

    added length

--- pythonbeyond/beyondclasses/shipping.py
+++ pythonbeyond/beyondclasses/shipping.py
@@ -1,23 +1,27 @@
 import iso6346
 '''
 ShippingContainer class 
-To demonstrate static methods,class methods,factory constructors
-
+To demonstrate static methods,class methods,factory constructors,properties
 '''
+
+
 class ShippingContainer:
 
-    next_serial =2109
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
 
-    @staticmethod 
-    def make_bic_code(owner_code,serial):
+    next_serial = 2109
+    container_format = "<owner_code={0};contents={1}=bic_code:{2};volume={3}>"
+
+    @staticmethod
+    def make_bic_code(owner_code, serial):
         """Returns an ISO 6346 shipping container code.
         Args:
         owner_code (str): Three character alphabetic container code.
         serial (str): Six digit numeric serial number.
         """
-        return iso6346.create(owner_code,serial=str(serial).zfill(6))
-   
-        
+        return iso6346.create(owner_code, serial=str(serial).zfill(6))
+
     @classmethod
     def get_next_serial(cls):
         """Gets  next serial number
@@ -25,33 +29,107 @@ class ShippingContainer:
         None
 
         """
-        result  = cls.next_serial
-        cls.next_serial  += 1
+        result = cls.next_serial
+        cls.next_serial += 1
         return result
 
     @classmethod
-    def create_empty(cls,owner_code):
+    def create_empty(cls, owner_code, length_ft, *args, **kwargs):
         """ Creates Shipping Container with empty contents.
         Args:
         owner_code (str): Three character alphabetic container code.
         """
-        return cls(owner_code,contents =None)
+        return cls(owner_code, length_ft, contents=None, *args, **kwargs)
 
     @classmethod
-    def create_with_items(cls,owner_code,items):
+    def create_with_items(cls, owner_code, length_ft, items, *args, **kwargs):
         """ Creates Shipping Container with list of contents.
         Args:
         owner_code (str): Three character alphabetic container code.
         items:list of items
-        
-        """
-        return cls(owner_code,contents =list(items))
 
-    def __init__(self,owner_code,contents):
+        """
+        return cls(owner_code, length_ft, contents=list(items), *args, **kwargs)
+
+    def __str__(self):
+        return ShippingContainer.container_format.format(
+            self.owner_code,
+            self.contents,
+            self.bic_code, self.volume_ft3)
+
+    def __init__(self, owner_code, length_ft, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.bic_code =ShippingContainer.make_bic_code(
-            owner_code =self.owner_code,
+        self.length_ft = length_ft
+        self.bic_code = self.make_bic_code(
+            owner_code=self.owner_code,
             serial=ShippingContainer.get_next_serial())
 
-    
+    @property
+    def volume_ft3(self):
+        return (ShippingContainer.HEIGHT_FT *
+                ShippingContainer.WIDTH_FT * self.length_ft)
+
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME_FT3 = 100
+
+    @staticmethod
+    def make_bic_code(owner_code, serial):
+        """Returns an ISO 6346 shipping container code.
+        Args:
+        owner_code (str): Three character alphabetic container code.
+        serial (str): Six digit numeric serial number.
+        """
+        return iso6346.create(owner_code,
+                              serial=str(serial).zfill(6),
+                              category="R")
+
+    @staticmethod
+    def _c_to_f(celsius):
+        return celsius * 9 / 5 + 32
+
+    @staticmethod
+    def _f_to_c(fahrenhiet):
+        return (fahrenhiet - 32) * 5 / 9
+
+    def __init__(self, owner_code, length_ft, contents, celsius):
+        super().__init__(owner_code, length_ft, contents)
+        self.celsius = celsius
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    @celsius.setter
+    def celsius(self, value):
+        if value > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature exceed {0}"
+                             .format(RefrigeratedShippingContainer))
+        self._celsius = value
+
+    @property
+    def fahrenhiet(self):
+        return RefrigeratedShippingContainer._c_to_f(self.celsius)
+
+    @fahrenhiet.setter
+    def fahrenhiet(self, value):
+        self.celsius = RefrigeratedShippingContainer._f_to_c(value)
+
+    @property
+    def volume_ft3(self):
+        return (super().volume_ft3 -
+                RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3)
+
+
+class HeatedRefrigeratedShippingContainer(RefrigeratedShippingContainer):
+
+    MIN_CELSIUS = -20.0
+
+    @RefrigeratedShippingContainer.celsius.setter
+    def celsius(self, value):
+        if value < HeatedRefrigeratedShippingContainer.MIN_CELSIUS:
+            raise ValueError("Temperature too cold!")
+        RefrigeratedShippingContainer.celsius.fset(self, value)
--- pythonbeyond/beyondclasses/shippingexample.py
+++ pythonbeyond/beyondclasses/shippingexample.py
@@ -1,23 +1,38 @@
-from shipping import ShippingContainer
+from shipping import (ShippingContainer, RefrigeratedShippingContainer,
+                      HeatedRefrigeratedShippingContainer)
 
 
-def print_container_details(container,print_format):
-      print(print_format.format(
+def print_container_details(container, print_format):
+    print(print_format.format(
         container.owner_code,
         container.contents,
         container.bic_code))
 
 
 if __name__ == '__main__':
-    container_format ="[owner_code:{0} ; contents:{1} ;bic_code:{2}]"
-    emtpy_containter = ShippingContainer.create_empty("KRI")
+    container_format = "[owner_code:{0} ; contents:{1} ;bic_code:{2}]"
+    emtpy_containter = ShippingContainer.create_empty("KRI", 100)
     print("Below are the empty container details")
-    print_container_details(emtpy_containter,container_format)
+    print_container_details(emtpy_containter, container_format)
     items_containter = ShippingContainer.create_with_items(
-        "KRI",
-        ["laptops","mobilephones"])
+        "KRI", 100,
+        ["laptops", "mobilephones"])
     print("Below are the items_containter details")
-    print_container_details(items_containter,container_format)
-    simple_containter = ShippingContainer("SWA","Footwear")
-    print_container_details(simple_containter,container_format)
-  
+    print_container_details(items_containter, container_format)
+    simple_containter = ShippingContainer("SWA", 100, "Footwear")
+    print_container_details(simple_containter, container_format)
+
+    empty_rfc = RefrigeratedShippingContainer.create_empty("ICE", 100,
+                                                           celsius=3.0)
+
+    print_container_details(empty_rfc, container_format)
+    print("change self.bic_code = ShippingContainer.make_bic_code in base class\
+,see the difference in subclass's bic_code")
+
+    items_rfc = RefrigeratedShippingContainer.create_with_items(
+        "ICE", 100,
+        ["fish", "peas"], celsius=3.0)
+    print("Below are the items_containter details")
+    print(items_rfc)
+    heatedrfc = HeatedRefrigeratedShippingContainer("HRC", 100,
+                                                    "PRAWNS", celsius=-18)
